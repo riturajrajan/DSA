e0c49aa81494e0bc10e75a5ed6d9a06e4ce72088
riturajrajan <prita.rituraj@gmail.com>
2023-12-02 17:16:43 +0530

    Peak element greater than the neighbor second approach

--- Arrays/src/PeakElement.java
+++ Arrays/src/PeakElement.java
@@ -1,17 +1,20 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class PeakElement {
     public static void main(String[] args) {
 
+        // First Approach
+
         /* Three cases can be arrived
          * 1. If array has one length
          * 2. If array has first element as greater or last element as greater. This is handled in line 27 and 39
-         * 3. If array has more than 3 elements then it will pass through all condition but if the elements is greater
+         * 3. If array has more than 3 elemets then it will pass through all condition but if the elements is greater
          *    in the middle will be decided by while()
          */
-        int[] neighbours = {10, 20, 5};
+        int[] neighbours = {40, 30, 20 , 10};
         int prevIndex = 0, currIndex = 0, nextIndex = 1;
         int arrLen = neighbours.length;
         List<Integer> output = new ArrayList<Integer>();
@@ -41,5 +44,31 @@ public class PeakElement {
             output.add(neighbours[arrLen-1]);
         }
         System.out.println(output);
+
+        //Second Approach
+        /*
+         * The main of this approach is to make an array with 3 elements and keep the first & last element as the
+         * smallest element in the array and then the code flows like above while loop in line 34
+         */
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter size of array");
+        int N = scan.nextInt();
+        int[] arr = new int[N + 2];
+        /* set corner values to -infinity */
+        arr[0] = Integer.MIN_VALUE;
+        arr[N + 1] = Integer.MIN_VALUE;
+
+        /* Accept N elements */
+        System.out.println("Enter "+ N +" elements");
+        for (int i = 1; i <= N; i++)
+            arr[i] = scan.nextInt();
+
+        /* Find All Peak Elements */
+        System.out.println("\nAll Peak Elements : ");
+        for (int i = 1; i <= N; i++)
+            if (arr[i - 1] <= arr[i] && arr[i] >= arr[i + 1])
+                System.out.println(arr[i] +" at position "+ i);
+
+        System.out.println();
     }
 }
